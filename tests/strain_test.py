68d1a1831a1ef7c3db8085b0c5eee8f4a2af5f3e
Patrick Schäfer <patrick.schaefer@informatik.hu-berlin.de>
2023-04-16 10:50:38 +0200

    tests

--- tests/strain_test.py
+++ tests/strain_test.py
@@ -12,20 +12,21 @@ warnings.simplefilter("ignore")
 def test_strain():
     ds_name = "Strain"
 
-    data = pd.read_csv("../datasets/experiments/strain-0-left.csv", index_col=0, squeeze=True)
+    data = pd.read_csv("../datasets/experiments/strain-0-left.csv",
+                       index_col=0, squeeze=True)
     print("Dataset Original Length n: ", len(data))
     data, factor = ml._resample(data, sampling_factor=10000)
     data[:] = zscore(data)
 
     k_max = 20
-    #length_range = np.arange(40, 100, 1)
-    #motif_length = plot_motif_length_selection(
-    #    k_max,
-    #    data.values,
-    #    length_range,
-    #    "left-0"
-    #)
-    #print (motif_length)
+    length_range = np.arange(40, 100, 10)
+    motif_length = plot_motif_length_selection(
+        k_max,
+        data.values,
+        length_range,
+        "left-0"
+    )
+    print (motif_length)
 
     motif_length = 100
     dists, motiflets, elbow_points = plot_elbow(
